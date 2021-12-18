4ae88e5aa01037e6bde601a0275e1dd6dbab00ae
Matias <matias.abarca2018@gmail.com>
2021-12-18 16:23:17 -0300

    Se agrega chart

--- ajax.js
+++ ajax.js
@@ -2,7 +2,7 @@
 export class Ajax{
 
     api_token = '117691097412738';
-
+    input = '';
     constructor(){
         this.escucharClick();
     }
@@ -10,8 +10,15 @@ export class Ajax{
     escucharClick(){
         
         $('.btn').click( () =>  {
-            const input = document.querySelector('.form-control');
-            this.llamadaAjax( input.value );
+            this.input = document.querySelector('.form-control');
+            if( this.input.value > 0 ) this.llamadaAjax( this.input.value );
+            else{
+                document.querySelector('.alert').classList.remove('d-none');  
+                this.input.value = '';
+                setTimeout(() => {
+                    document.querySelector('.alert').classList.add('d-none');  
+                }, 2000);
+            } 
         });
     }
     crearCard( heroe ){
@@ -21,17 +28,22 @@ export class Ajax{
         const cardBody = document.createElement('div');
         const { image, name, appearance, biography } = heroe;
         const { race, height, weight } = appearance;
+        // Limpiar HTML
+        divAppend.innerHTML = '';
+
         // Modificando card
-        card.style.width = '35rem';
-        cardBody.classList.add('card.body');
-        cardBody.innerHTML = `<h3 class="card-title">${ name }</h3>
+        card.classList.add('d-flex')
+        cardBody.classList.add('card-body','pt-0');
+        cardBody.innerHTML = `
+        <div>
+        <h3 class="card-title border-bottom border-primary">${ name }</h3>
         <ul class="list-group">
             <li class="list-group-item">Nombre Actor: <strong>${ biography['full-name']}</strong> </li>
             <li class="list-group-item">Primera aparicion: <strong>${ biography['first-appearance']}</strong> </li>
             <li class="list-group-item">Raza: <strong>${ ( race == 'null' ) ? 'Sin info': race }</strong> </li>
-            <li class="list-group-item">Altura: <strong>${ (height[1] == '0 cm') ? 'Sin info': height}</strong></li>
+            <li class="list-group-item">Altura: <strong>${ (height[1] == '0 cm') ? 'Sin info': height[1]}</strong></li>
             <li class="list-group-item">Peso: <strong>${ ( weight[1] == '0 kg' ) ? 'Sin info': weight[1]}</li>
-        </ul>`;
+        </ul> </div>` ;
         // modificar img
         img.src = image.url;
         img.classList.add('card-img-top');
@@ -42,10 +54,10 @@ export class Ajax{
         divAppend.append(card);
     }
     
-    async llamadaAjax( busqueda ){
+    llamadaAjax( busqueda ){
         const spinner = document.querySelector('.spinner-border');
         spinner.classList.remove('d-none')
-        await $.ajax({
+        $.ajax({
             type:"GET",
             url:`https://superheroapi.com/api/${ this.api_token }/${ busqueda } `,
             dataType:"json",
@@ -60,10 +72,35 @@ export class Ajax{
             },
             async: true,
            }).then( resp =>{
-                this.crearCard( resp ) 
-                spinner.classList.add('d-none'); 
+                this.input.value = '';
+               spinner.classList.add('d-none'); 
+                this.crearCard( resp );
+                this.crearGrafica( resp );
             }  ) ;
     }
-    
-
+    crearGrafica( heroe ){
+        const { powerstats } = heroe;
+        if( powerstats.intelligence === 'null' &&  powerstats.strength === 'null'){
+            
+            return;
+        }
+        const chart =  new CanvasJS.Chart('chartContainer',{
+            theme: 'light2',
+            title:{
+                text: 'Estadisticas de poder'
+            },
+            data: [
+                {
+                    type:"doughnut",
+                    dataPoints: [
+                        { label: "Inteligencia", y: Number(powerstats.intelligence) },
+                        { label: "Fuerza", y: Number(powerstats.strength) },
+                        { label: "Velocidad", y: Number(powerstats.speed) },
+                        { label: "Poder", y: Number(powerstats.power)  },
+                    ]
+                }
+            ]
+        });
+        chart.render();
+    }
 }
